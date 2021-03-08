2a56ed52a54606bbf9e029d022f9cabfe43d8ffd
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-08 20:36:41 +0700

    Create Using arguments in Bash scripts.sh

---
+++ 1.From Command-Line to Bash Script/Using arguments in Bash scripts.sh
@@ -0,0 +1,11 @@
+# Echo the first and second ARGV arguments
+echo $1 
+echo $2
+
+# Echo out the entire ARGV array
+echo $@
+
+# Echo out the size of ARGV
+echo $#
+
+repl:~/workspace$ bash script.sh Bird Fish Rabbit
