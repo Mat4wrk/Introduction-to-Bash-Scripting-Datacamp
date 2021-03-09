5f47776354df9fb1c33d2e03decd8180f1e5dcce
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-09 18:50:11 +0700

    Update Moving relevant files.sh

--- 3.Control Statements in Bash Scripting/Moving relevant files.sh
+++ 3.Control Statements in Bash Scripting/Moving relevant files.sh
@@ -6,3 +6,7 @@ if grep -q 'SRVM_' $sfile && grep -q 'vpt' $sfile; then
 	# Move file if matched
 	mv $sfile good_logs/
 fi
+
+repl:~/workspace$ bash script.sh logfiles8.txt
+repl:~/workspace$ bash script.sh log1.txt
+repl:~/workspace$ bash script.sh logdays.txt
