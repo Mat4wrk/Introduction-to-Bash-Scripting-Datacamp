afac45ec244f06f26c04d19153835d44406241c5
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-09 18:57:07 +0700

    Create Cleaning up a directory.sh

---
+++ 3.Control Statements in Bash Scripting/Cleaning up a directory.sh
@@ -0,0 +1,12 @@
+# Create a FOR statement on files in directory
+for file in robs_files/*.py
+do  
+    # Create IF statement using grep
+    if grep -q 'RandomForestClassifier' $file ; then
+        # Move wanted files to to_keep/ folder
+        mv $file to_keep/
+    fi
+done
+
+
+repl:~/workspace$ bash script.sh code_file101.py
